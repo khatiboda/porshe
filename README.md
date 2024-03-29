1379beb91dbad19cb08076bd205a2d929e7cef2b
vladimir snape <109045402+VladSnape@users.noreply.github.com>
2024-03-29 06:18:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Anix
+ anix motors 
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    darkMode: 'class',
+    extend: {
+      fontFamily:{
+        'Poppins':[ "Poppins"],
+        'Lobster':[ "Lobster"]
+      }
+    },
+  },
+  plugins: [],
+}
+
